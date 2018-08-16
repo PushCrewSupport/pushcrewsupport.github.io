e9704ba56ba7c5d71af0b004046f5a1be003eb24
PushCrew Tech Support <31190055+PushCrewSupport@users.noreply.github.com>
2018-08-16 13:24:19 +0530

    Update ffsw-pushcrew.js

--- ffsw-pushcrew.js
+++ ffsw-pushcrew.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/ffsw-81ce094bad848c6dc0a004ef76465713.js');
+importScripts('https://cdn.pushcrew.com/sw/ffsw-95d9f80c45f232d426fd707a64cf75c1.js');
