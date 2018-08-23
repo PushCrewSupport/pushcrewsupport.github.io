0eb973605fda927355609f1d793ec382c71c49dd
PushCrew Tech Support <31190055+PushCrewSupport@users.noreply.github.com>
2018-08-23 15:39:41 +0530

    Add files via upload

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/889002d89f3e5cc08bcbc4529617ffe5.js');
+importScripts('https://cdn.pushcrew.com/sw/8a821bc2cbf301fc3bec17f55f16ab11.js');
