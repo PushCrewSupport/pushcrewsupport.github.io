8356662135b75100d812fa2565cc1c12f1d773bf
PushCrew Tech Support <31190055+PushCrewSupport@users.noreply.github.com>
2018-09-26 20:31:43 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/664b034f59574e0234e02a4e6e7c3133.js');
+importScripts('https://cdn.pushcrew.com/sw/172cf6181f2babc9fd41e0aa20e0cbc8.js');
