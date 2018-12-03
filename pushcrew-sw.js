ba797efab9948810d56bf4989ff2e8012050f246
PushCrew Tech Support <31190055+PushCrewSupport@users.noreply.github.com>
2018-12-03 14:14:52 +0530

    Update pushcrew-sw.js

--- pushcrew-sw.js
+++ pushcrew-sw.js
@@ -1,2 +1,2 @@
 var version = 6;
-importScripts('https://cdn.pushcrew.com/sw/28bf064f523b504dba3292e914be454e.js');
+importScripts('https://cdn.pushcrewstaging.com/sw/28bf064f523b504dba3292e914be454e.js');
